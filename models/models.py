423e2f7d2d269ab0d8c0116068185d8e7945b3bc
Yura Kholodov <yurdast@mail.ru>
2024-01-29 15:05:44 +0300

    db added

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import JSON, TIMESTAMP, ForeignKey
+from sqlalchemy import JSON, TIMESTAMP, ForeignKey, Boolean
 
 
 class Base(DeclarativeBase):
@@ -11,17 +11,24 @@ class Base(DeclarativeBase):
 
 
 class Role(Base):
-    __tablename__ = 'roles'
+    __tablename__ = 'role'
 
     name: Mapped[str] = mapped_column(nullable=False)
     permissions: Mapped[str] = mapped_column(JSON)
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     email: Mapped[str] = mapped_column(nullable=False)
     username: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
     registered_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.utcnow)
-    role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'), nullable=True)
+    role_id: Mapped[int] = mapped_column(ForeignKey('role.id'), nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
